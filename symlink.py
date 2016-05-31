51fdc44402cc0b9265e0d96cb043e301f13161bb
Mausam Yede <mausam.yede1@gmail.com>
2016-06-01 02:30:37 +0530

    Add files via upload

---
+++ symlink.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+
+#This script only detects and removes softlinks. Other types of duplicate files wont be affected. Currently working on them too. 
+ 
+import os
+from os import listdir
+import os.path
+
+links = ["" for x in range(1000)]
+count=0
+def scanvol():
+	global links
+	global count
+	for i in os.listdir(os.getcwd()):
+		if os.path.islink(i):
+			print i,' in directory: ',os.getcwd()
+			print ''
+			links[count] = os.getcwd()+"/"+i
+			count=count+1
+		if os.path.isdir(i):
+			os.chdir(i)
+			scanvol()
+	os.chdir('..')
+
+def purge():
+	for x in range(count):
+		os.system('rm '+links[x])
+		
+vol_path = raw_input("Enter the path of the volume to be scanned: ")
+os.chdir(vol_path)
+print 'Following links are present in the current volume: '
+scanvol()
+
+while True:
+	choice = raw_input("Do you want to remove these links(Y/n): ")
+	if choice == "Y" or "y":
+		purge()
+		break
+	elif choice == "n":
+		break
